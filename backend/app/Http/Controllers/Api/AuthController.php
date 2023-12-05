df3be3ccddde28f5cf213ddc7623339bb39f8b10
Fabián Bugueño Flores <fabianb175@gmail.com>
2023-12-05 18:38:29 -0300

    Intento de login

---
+++ backend/app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Usuario;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $usuario = Usuario::where('usuario', $request->username)->first();
+        $foo = Hash::check($request->password, $usuario->password);
+        if (!$usuario || !Hash::check($request->password, $usuario->password)) {
+            return response([
+                'message' => ['Estas credenciales no coinciden con nuestros registros.']
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+        $response = [
+
+            'usuarioId' => $usuario->_id,
+            'usuario' => $usuario->rol,
+        ];
+        return response($response, Response::HTTP_OK);
+    }
+    public function checkSession(Request $request)
+    {
+        $usuario = Usuario::where('_id', $request->usuarioId)->first();
+        if (!$usuario) {
+            return response([
+                'message' => ['No se encontro usuario.']
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+        $response = [
+
+            'usuarioId' => $usuario->_id,
+            'usuario' => $usuario->rol,
+        ];
+        return response($response, Response::HTTP_OK);
+    }
+}
---
+++ frontend/src/SGI/Login/Login.jsx
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from "react";
+import "./Login.css";
+import usePostRequest from "../Hooks/usePostRequest";
+import { useNavigate } from "react-router-dom";
+
+export const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const { execute, response } = usePostRequest();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = {
+      username: username,
+      password: password,
+    };
+    const url = "login";
+    let response = await execute(data, url);
+
+    if (response) {
+      localStorage.setItem("response", JSON.stringify(response));
+      console.log("Login success");
+      navigate("/");
+    }
+  };
+
+  return (
+    <>
+      <div className="container mt-5">
+        <div className="row justify-content-center">
+          <div className="col-md-6">
+            <div className="card">
+              <div className="card-header bg-primary text-white text-center">
+                <h2>Iniciar sesión</h2>
+              </div>
+              <div className="card-body">
+                <form onSubmit={handleSubmit}>
+                  <div className="col">
+                    <label
+                      htmlFor="username"
+                      className="col-auto col-form-label"
+                    >
+                      Usuario
+                    </label>
+                    <input
+                      type="text"
+                      id="usernameInput"
+                      value={username}
+                      onChange={(e) => setUsername(e.target.value)}
+                      className="form-control"
+                    />
+                  </div>
+
+                  <div className="col">
+                    <label
+                      htmlFor="password"
+                      className="col-auto col-form-label"
+                    >
+                      Contraseña
+                    </label>
+                    <input
+                      type="password"
+                      id="passwordInput"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      className="form-control"
+                    />
+                  </div>
+                  <button className="btn btn-primary mt-3 w-100" type="submit">
+                    Confirmar
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
