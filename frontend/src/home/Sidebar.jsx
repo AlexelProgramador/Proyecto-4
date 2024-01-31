b0e99fe52c554e7aa98c151c2e9c455b54fc97b6
araeri <aracely.leona1@gmail.com>
2024-01-31 10:07:08 -0300

    Update Sidebar.jsx

--- frontend/src/home/Sidebar.jsx
+++ frontend/src/home/Sidebar.jsx
@@ -116,18 +116,12 @@ export const Sidebar = () => {
                 onMouseLeave={() => setIsHoveredMySoli(false)}
                 className="d-flex nav-link"
               >
-                {/* <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  x="0px"
+                <svg xmlns="http://www.w3.org/2000/svg"  x="0px"
                   y="0px"
-                  width="40"
-                  height="40"
-                  fill="currentColor"
-                  class="bi bi-file-earmark"
-                  viewBox="3 0 8 19"
-                >
-                  <path d="M14 4.5V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h5.5L14 4.5zm-3 0A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5h-2z" />
-                </svg> */}
+                  width="36"
+                  height="36" fill="currentColor" class="bi bi-files-alt" viewBox="3 0 8 19">
+                  <path d="M11 0H3a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2 2 2 0 0 0 2-2V4a2 2 0 0 0-2-2 2 2 0 0 0-2-2m2 3a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1zM2 2a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1z"/>
+                </svg>
                 <span className="ml-auto">Solicitudes</span>
               </div>
               )}
@@ -196,15 +190,10 @@ export const Sidebar = () => {
                 onMouseLeave={() => setIsHoveredMySoli(false)}
                 className="d-flex nav-link"
               >
-                <svg xmlns="http://www.w3.org/2000/svg"  
-                  x="0px"
-                  y="0px"
-                  width="20"
-                  height="20"
-                  fill="currentColor"
-                  class="bi bi-file-earmark"
-                  viewBox="0 0 448 512">
-                <path d="M304 128a80 80 0 1 0 -160 0 80 80 0 1 0 160 0zM96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM49.3 464H398.7c-8.9-63.3-63.3-112-129-112H178.3c-65.7 0-120.1 48.7-129 112zM0 482.3C0 383.8 79.8 304 178.3 304h91.4C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7H29.7C13.3 512 0 498.7 0 482.3z"/></svg>
+                <svg xmlns="http://www.w3.org/2000/svg"  x="0px"
+                  y="0px" width="35" height="35" fill="currentColor" class="bi bi-people" viewBox="3 0 8 19">
+                  <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1zm-7.978-1L7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002-.014.002zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4m3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0M6.936 9.28a6 6 0 0 0-1.23-.247A7 7 0 0 0 5 9c-4 0-5 3-5 4q0 1 1 1h4.216A2.24 2.24 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816M4.92 10A5.5 5.5 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0m3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4"/>
+                </svg>                
                 <span className="ml-auto">Usuarios</span>
               </div>
               )}
